298b11a7e99e966dd9ae1e2ff07d21f1f64eb697
Ayan Biswas <ayanbiswas184@gmail.com>
2020-04-01 14:28:08 +0530

    autoplay jquery moved to init.js

--- assets/js/init.js
+++ assets/js/init.js
@@ -5,3 +5,19 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+//Initialization for the autoplay of sliders
+$( window ).on( "load", function() {
+  console.log( "window loaded" );
+  $('.carousel.carousel-slider').carousel({
+fullWidth: true,
+indicators: true
+});
+autoplay();
+function autoplay(){
+$('.carousel').carousel('next');
+setTimeout(autoplay,3500);
+}
+
+});
+//Initialization for the sliders end
